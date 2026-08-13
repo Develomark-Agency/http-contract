5167353f1322d9401f35066e384f53bbf8ea9d28
jclarkDM <jclark@develomark.com>
2026-08-13 17:30:43 -0400

    Fixed missing `constantQuery` export

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+
 /** Public APIs for defining and calling typed HTTP endpoints. */
 export { APIConnector } from "./src/api";
 export { OpenAIJSONSchemaAdapter } from "./src/openai-json-schema";
@@ -5,6 +6,7 @@ export { OpenAIJSONSchemaAdapter } from "./src/openai-json-schema";
 export { method, type Method } from "./src/endpoint-modifiers/method";
 export { path, type PathModifier } from "./src/endpoint-modifiers/path";
 export { query } from "./src/endpoint-modifiers/query";
+export { constantQuery } from "./src/endpoint-modifiers/constant-query";
 export { requestBody } from "./src/endpoint-modifiers/request-body";
 export { requestHeaders } from "./src/endpoint-modifiers/request-headers";
 export { responseBody } from "./src/endpoint-modifiers/response-body";
